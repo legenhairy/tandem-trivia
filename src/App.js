e9e44d7f7827a04239c2fd3a21d258abef08b293
Harry Zhang <harryzhang100@gmail.com>
2020-10-30 15:39:37 -0700

    user can only make one selection, correct and incorrect answers are shown visually after selection

--- src/App.js
+++ src/App.js
@@ -6,23 +6,38 @@ function App() {
   const [questionIndex, setQuestionIndex] = useState(0);
   const [questions, setQuestions] = useState([]);
   const [score, setScore] = useState(0);
+  const [userSelected, setUserSelected] = useState(false);
 
   useEffect(() => {
     setQuestions(data);
   }, []);
 
   const handleAnswer = (answer) => {
-    setQuestionIndex(questionIndex + 1);
+    //setQuestionIndex(questionIndex + 1);
     
-    //add to the score
-    if(answer === questions[questionIndex].correct) {
-      setScore(score + 1);
+    //we don't want user to be able to choose more than one choice for a question
+    
+    if(!userSelected) {
+      if(answer === questions[questionIndex].correct) {
+        setScore(score + 1);
+      }
     }
+    //user made a choice, now answer is locked in and we can see the correct/incorrect answers
+    setUserSelected(true);
+
   }
+
   //need to wait for the questions to load in
   return questions.length > 0 ? (
     <div className='container'>
-      <Question questionData={data[questionIndex]} handleAnswer={handleAnswer} />
+      {
+        questionIndex >= questions.length ? (
+          <h1 className='text-3xl text-white font-bold'>
+            Your score is {score}
+          </h1>
+        ) :(
+          <Question questionData={data[questionIndex]} handleAnswer={handleAnswer} userSelected={userSelected} />
+        )}
     </div>
   ): (
     <h2 className='text-2xl tetx-white font-bold'>Loading Questions...</h2>
--- src/components/Question.js
+++ src/components/Question.js
@@ -3,7 +3,7 @@ import Answer from './Answer';
 
 //before, i would have to write data[questionIndex].correct, now i can just write correct
 //since i can dereference object props, every question component should just take in the object
-const Question = ({ handleAnswer, questionData: { question, correct, incorrect }, }) => {
+const Question = ({ userSelected, handleAnswer, questionData: { question, correct, incorrect }, }) => {
   //shuffle the order of the answers so that i wont have the same order every time
   const shuffledAnswers = [correct, ...incorrect].sort(() => Math.random() - 0.5);  
   return (
@@ -14,16 +14,28 @@ const Question = ({ handleAnswer, questionData: { question, correct, incorrect }
           </h2>
         </div>
         <div className='flex flex-col mt-8'>
-          <Answer className={correct === shuffledAnswers[0] ? 'bg-purple-300' : ''} 
+          <Answer className={userSelected 
+            ? shuffledAnswers[0] === correct 
+              ? 'bg-green-400': 'bg-red-400'
+            : 'bg-white'} 
             handleAnswer={() => handleAnswer(shuffledAnswers[0])} 
             answer={shuffledAnswers[0]} />
-          <Answer className={correct === shuffledAnswers[1] ? 'bg-purple-300' : ''}
+          <Answer className={userSelected 
+            ? shuffledAnswers[1] === correct 
+              ? 'bg-green-400': 'bg-red-400'
+            : 'bg-white'}
             handleAnswer={() => handleAnswer(shuffledAnswers[1])} 
             answer={shuffledAnswers[1]} />
-          <Answer className={correct === shuffledAnswers[2] ? 'bg-purple-300' : ''} 
+          <Answer className={userSelected 
+            ? shuffledAnswers[2] === correct 
+              ? 'bg-green-400': 'bg-red-400'
+            : 'bg-white'} 
             handleAnswer={() => handleAnswer(shuffledAnswers[2])} 
             answer={shuffledAnswers[2]} />
-          <Answer className={correct === shuffledAnswers[3] ? 'bg-purple-300' : ''}
+          <Answer className={userSelected 
+            ? shuffledAnswers[3] === correct 
+              ? 'bg-green-400': 'bg-red-400'
+            : 'bg-white'}
             handleAnswer={() => handleAnswer(shuffledAnswers[3])} 
             answer={shuffledAnswers[3]} />
         </div>
